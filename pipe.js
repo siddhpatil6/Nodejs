4e7776463cae6a86157216dbeb1091ac863cc410
siddhantpatil <siddhpatil6@gmail.com>
2018-11-06 18:30:02 +0530

    git changes

--- pipe.js
+++ pipe.js
@@ -6,7 +6,6 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
 
-  res.write('Hello World!'); //write a response to the client
   var readStream=fs.createReadStream(__dirname+"/readMe.txt",'utf-8')
   var writeStream=fs.createWriteStream('writeMe.txt')
 
